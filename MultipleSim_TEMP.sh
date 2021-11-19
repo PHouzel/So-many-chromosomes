d5a70fcf327289acb2f2a82cc30d7ca9cd3af1c4
lfdavoli <lorenzo.davoli3@studio.unibo.it>
2021-11-19 14:57:21 +0100

    Some analysis files

--- MultipleSim_TEMP.sh
+++ MultipleSim_TEMP.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-N_ITER=5
+# run multiple simulation with these parameters
+N_ITER=2
 SIM_NAME=run2-1000
-DIR_NAME=dumplin1000_A100_cluster
-DUMPLIN_NAME=dumplin1000_K100
+DIR_NAME=dumplin_cluster
+DIR_NAME_IMAGE=dumplin_cluster_image
+DUMPLIN_NAME=dumplin
+DUMPLIN_NAME_IMAGE=dumplin_image
+py_NAME=self-avoid.py
+in_NAME=polymer-descript.in
 
+bar=_
 echo "Arguments: "
 echo $SIM_NAME
 echo $DIR_NAME
-echo $DUMPLIN_NAME
+echo $py_NAME
+echo $in_NAME
+
 
 cd $SIM_NAME
 mkdir $DIR_NAME
+mkdir $DIR_NAME_IMAGE
 
-for iter in {1..$N_ITER}
+for (( iter=1; iter<=2; iter++ ))
 do
-	python3 self-avoid.py
-   mpirun -n 6 lmp -in polymer-descript.in 
-   cp -R dumplin $DIR_NAME
+   # Execute
+	python3 $py_NAME
+   mpirun -n 6 lmp -in $in_NAME
+   # Move output files
    cd $DIR_NAME
-   mv dumplin $DUMPLIN_NAME_$iter
+   mkdir $DUMPLIN_NAME$bar$iter
    cd ..
+   cd $DIR_NAME_IMAGE
+   mkdir $DUMPLIN_NAME_IMAGE$bar$iter
+   cd ..
+
+   cp -R dumplin $DIR_NAME/$DUMPLIN_NAME$bar$iter
+   cp -R dumplin_image $DIR_NAME_IMAGE/$DUMPLIN_NAME_IMAGE$bar$iter
+
+   # Delete old run files
+   rm dumplin/*
+   rm dumplin_image/*
 done
 
 echo "Everything went fine, I take a nap now."
